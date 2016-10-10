7e76c1d403bffeea436308882a87de805f69546e
Michal  Korzeniak <2korzeniak@stud110>
2016-10-10 18:26:50 +0200

    3 commit

--- symulator.py
+++ symulator.py
@@ -2,13 +2,12 @@ import random
 import time
 
 def main():
-  
-  
+  Y=0;
   while 1:
     X = random.gauss(-360,360)
-    Y = random.gauss(-360,360)    
-    print "Current angle: " + str(X)
-    print "Applied course: " + str(Y)
+    Y = Y-X    
+    print "Current angle: " + str(Y)
+    print "Applied course: " + str(X)
     time.sleep(1)
     
 main()
